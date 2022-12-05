cc0b1dbcb6e94a30ef63c3efe66ea868d3943cbf
김민찬 <81581362+nsmchan@users.noreply.github.com>
2022-12-05 09:56:51 +0900

    예약 페이지 수정중(#2) 층 클릭시 이미지 로드

--- app/src/main/java/com/kosa/thirdprojectfront/ReservationActivity.kt
+++ app/src/main/java/com/kosa/thirdprojectfront/ReservationActivity.kt
@@ -2,10 +2,24 @@ package com.kosa.thirdprojectfront
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kosa.thirdprojectfront.databinding.ActivityReservationBinding
 
 class ReservationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReservationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation)
+        binding = ActivityReservationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnExpand.setOnClickListener {
+            if (binding.expandLayout.visibility == View.VISIBLE) {
+                binding.expandLayout.visibility = View.GONE
+            } else {
+                binding.expandLayout.visibility = View.VISIBLE
+            }
+        }
     }
 }
