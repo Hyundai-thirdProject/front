df1c590cf9dc00ba6d2213293ffa0aaf5f1440c7
wkdwndus0409 <jjy990409@naver.com>
2022-12-06 10:05:52 +0900

    마이페이지(QR) (#26)

---
+++ app/src/main/java/com/kosa/thirdprojectfront/EmptyQRFragment.kt
@@ -0,0 +1,58 @@
+package com.kosa.thirdprojectfront
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kosa.thirdprojectfront.databinding.FragmentEmptyQRBinding
+
+class EmptyQRFragment : Fragment(), View.OnClickListener {
+
+    var activity: MainActivity? = null
+    private lateinit var binding : FragmentEmptyQRBinding
+
+    override fun onAttach(context: Context) {
+        if (context != null) {
+            super.onAttach(context)
+            activity = getActivity() as MainActivity?
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        activity = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentEmptyQRBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+
+        val imageView = binding.empty
+        imageView.setImageResource(R.drawable.empty)
+
+        imageView.setOnClickListener(this)
+        return view
+//        imageView.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                when (v?.id) {
+//                    R.id.empty -> {
+//                        activity?.onFragmentChange(0)
+//                    }
+//                }
+//            }
+//        })
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.empty -> {
+                Log.d("jjy", "EmptyQRFragment")
+                activity?.onFragmentChange(2)
+            }
+        }
+    }
+}
