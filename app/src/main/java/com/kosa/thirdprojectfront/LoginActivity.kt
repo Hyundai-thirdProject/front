3a07cb55c599d6142f3a597302dad9b059c5e9b8
shinmirim <90263843+shinmirim@users.noreply.github.com>
2022-12-07 20:06:41 +0900

    로그인 (네이버, 카카오)
    로그인 구현 #38

--- app/src/main/java/com/kosa/thirdprojectfront/LoginActivity.kt
+++ app/src/main/java/com/kosa/thirdprojectfront/LoginActivity.kt
@@ -16,26 +16,47 @@ import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.content.pm.PackageManager.NameNotFoundException
 import android.util.Base64
+import android.widget.ImageView
+import android.widget.Toast
 import com.kakao.auth.Session
 import com.kakao.util.helper.Utility
 import com.kosa.thirdprojectfront.SessionCallback
+import com.kosa.thirdprojectfront.databinding.ActivityLoginBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.api.NidProfileApi
+import com.navercorp.nid.profile.data.NidProfileResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var loginV1: Button
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var loginV1: ImageView
     private lateinit var logout: Button
-    //private val sessionCallback: PackageInstaller.SessionCallback? = PackageInstaller.SessionCallback
     private val sessionCallback : SessionCallback = SessionCallback()
     private lateinit var session: Session
+    private lateinit var loginV2: ImageView
+
+    private var email: String = ""
+    private var gender: String = ""
+    private var name: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         loginV1 = findViewById(R.id.loginV1)
         logout = findViewById(R.id.logout)
+        loginV2 = findViewById(R.id.tv_naver_login)
 
         session = Session.getCurrentSession()
         Log.d("fkfkfkfk", session.toString())
@@ -55,8 +76,48 @@ class LoginActivity : AppCompatActivity() {
                 Log.d("afafaf", session.toString())
             }
         })
+
+        loginV2.setOnClickListener {
+            val oAuthLoginCallback = object : OAuthLoginCallback {
+                override fun onSuccess() {
+                    // 네이버 로그인 API 호출 성공 시 유저 정보를 가져온다
+                    NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse> {
+                        override fun onSuccess(result: NidProfileResponse) {
+                            name = result.profile?.name.toString()
+                            email = result.profile?.email.toString()
+                            gender = result.profile?.gender.toString()
+                            Log.e(TAG, "네이버 로그인한 유저 정보 - 이름 : $name")
+                            Log.e(TAG, "네이버 로그인한 유저 정보 - 이메일 : $email")
+                            Log.e(TAG, "네이버 로그인한 유저 정보 - 성별 : $gender")
+                        }
+                        override fun onError(errorCode: Int, message: String) {
+                            //
+                        }
+                        override fun onFailure(httpStatus: Int, message: String) {
+                            //
+                        }
+                    })
+                }
+
+                override fun onError(errorCode: Int, message: String) {
+                    val naverAccessToken = NaverIdLoginSDK.getAccessToken()
+                    Log.e(TAG, "naverAccessToken : $naverAccessToken")
+                }
+                override fun onFailure(httpStatus: Int, message: String) {
+                    //
+                }
+            }
+
+            NaverIdLoginSDK.initialize(this@LoginActivity, getString(R.string.naver_client_id), getString(R.string.naver_client_secret), "앱 이름")
+            NaverIdLoginSDK.authenticate(this@LoginActivity, oAuthLoginCallback)
+        }
+
         logout.setOnClickListener(View.OnClickListener { v: View? ->
             Log.d(TAG, "onCreate:click ")
+
+            startNaverLogout()
+
+
             UserManagement.getInstance()
                 .requestLogout(object : LogoutResponseCallback() {
                     override fun onSessionClosed(errorResult: ErrorResult) {
@@ -116,4 +177,14 @@ class LoginActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "LoginActivity"
     }
+    /**
+     * 로그아웃
+     * 애플리케이션에서 로그아웃할 때는 다음과 같이 NaverIdLoginSDK.logout() 메서드를 호출합니다. */
+    private fun startNaverLogout(){
+        NaverIdLoginSDK.logout()
+        //setLayoutState(false)
+        Toast.makeText(this@LoginActivity, "네이버 아이디 로그아웃 성공!", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
